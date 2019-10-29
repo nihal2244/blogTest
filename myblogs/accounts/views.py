c70c06b82fa4945c068e1a019709c33e47604127
nihal2244 <nihal2244@yahoo.com>
2019-10-30 01:46:43 +0530

    updated requirement.txt

--- myblogs/accounts/views.py
+++ myblogs/accounts/views.py
@@ -4,7 +4,19 @@ from django.contrib import auth
 
 
 def signup(request):
-    return render(request,"accounts/signup.html")
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user=User.objects.get(username=request.POST['username'])
+                return render(request,'accounts/signup.html',{'error':'user already exist'})
+            except User.DoesNotExist:
+                user=User.objects.create_user(request.POST['username'],password=request.POST['password1']    )
+                auth.login(request,user)
+                return redirect('home')
+        else:
+            return render(request,"accounts/signup.html",{"error":"password must match"})
+    else:
+        return render(request,"accounts/signup.html")
 
 def login(request):
     if request.method=='POST':
