fbea10cb3c63cf9dddd64437087fdfee1f925aab
nihal2244 <nihal2244@yahoo.com>
2019-10-31 18:02:34 +0530

    added blog page and dash board

--- myblogs/blogs/models.py
+++ myblogs/blogs/models.py
@@ -5,32 +5,32 @@ from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=225)
-    body = models.TextField()
-    created_date = models.DateField(auto_now_add=True)
-    like_total = models.IntegerField()
-    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=225,null=True,blank=True)
+    body = models.TextField(null=True,blank=True)
+    posted_on =  models.DateTimeField(null=True)
+    like_total = models.IntegerField(default=0,blank=True,null=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
 
     def __str__(self):
         return self.title
-    def summary():
+    def summary(self):
         return self.body[:100]
-    def created_date():
-        return self.created_date.strftime('%b %e %Y')
+    def created_date(self):
+        return self.created_date.strftime("%b %e %Y")
 
     # class Meta:
     #     ordering = ['-created_date']
 
 
-class Comment(models.Model):
-    blog= models.ForeignKey(
-        Blog, on_delete=models.CASCADE, )
-    comment_author = models.CharField(max_length=50)
-    comment_content = models.CharField(max_length=200)
-    comment_date = models.DateTimeField(auto_now_add=True)
+# class Comment(models.Model):
+#     blog= models.ForeignKey(
+#         Blog, on_delete=models.CASCADE, )
+#     comment_author = models.CharField(max_length=50)
+#     comment_content = models.CharField(max_length=200)
+#     comment_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.comment_content
+#     def __str__(self):
+#         return self.comment_content
 
     # class Meta:
     #     ordering = ['-comment_date']
--- myblogs/blogs/views.py
+++ myblogs/blogs/views.py
@@ -1,26 +1,38 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Blog
 from django.utils import timezone
 # Create your views here.
+
+
 def home(request):
-    return render(request,'blogs/home.html')
+    return render(request, 'blogs/home.html')
+
 
 @login_required
 def create(request):
-    if request.method=="POST":    
-        if request.POST['title'] and request.POST['body']:
-            blog=Blog()
-            blog.title=request.POST['title']
-            # print(blog.title)
-            blog.body=request.POST['body']
-            print(blog.body)
-            blog.created_date=timezone.datetime.now()
-            blog.author=request.user
-            
-            return redirect('home')
-        else:
-            return render(request,"blogs/create.html",{"error":"Require all fields"})
+    if request.method == "POST":
+        try:
+            if request.POST['title'] and request.POST['body']:
+                print(request.POST['title'], request.POST['body'])
+                blog = Blog()
+                blog.title = request.POST['title']
+                # print(blog.title)
+                blog.body = request.POST['body']
+                print(blog.body)
+                blog.posted_on = timezone.datetime.now()
+                blog.author = request.user
+                print("author", blog.author)
+                blog.save()
+                return redirect("/blogs/" +str(blog.id))
+            else:
+                return render(request, "blogs/create.html", {"error": "Require all fields"})
+        except Exception as e:
+            print(error, e.message, e.args)
 
     else:
-        return render(request,"blogs/create.html")
+        return render(request, "blogs/create.html")
+@login_required
+def blog_details(request,blog_id):
+    blog=get_object_or_404(Blog,pk=blog_id)
+    return render(request,"blogs/blog-page.html",{"blog":blog} )
