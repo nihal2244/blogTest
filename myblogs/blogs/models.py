efc5ca2255cd16855350934a929d72adf885ce44
nihal2244 <nihal2244@yahoo.com>
2019-11-01 09:28:50 +0530

    modified comment section

--- myblogs/blogs/models.py
+++ myblogs/blogs/models.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
     posted_on =  models.DateTimeField(null=True)
     like_total = models.IntegerField(default=0)
     views_total = models.IntegerField(default=0)
-    author=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE,null=True,)
 
     def __str__(self):
         return self.title
@@ -19,19 +19,19 @@ class Blog(models.Model):
     def created_date(self):
         return self.created_date.strftime("%b %e %Y")
 
-    # class Meta:
-    #     ordering = ['-created_date']
+    class Meta:
+        ordering = ['-posted_on']
 
 
-# class Comment(models.Model):
-#     blog= models.ForeignKey(
-#         Blog, on_delete=models.CASCADE, )
-#     comment_author = models.CharField(max_length=50)
-#     comment_content = models.CharField(max_length=200)
-#     comment_date = models.DateTimeField(auto_now_add=True)
+class Comment(models.Model):
+    blog= models.ForeignKey(
+        Blog, on_delete=models.CASCADE,related_name="comments")
+    comment_author = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    comment_content = models.CharField(max_length=200)
+    comment_date = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.comment_content
+    def __str__(self):
+        return self.comment_content
 
-    # class Meta:
-    #     ordering = ['-comment_date']
+    class Meta:
+        ordering = ['-comment_date']
